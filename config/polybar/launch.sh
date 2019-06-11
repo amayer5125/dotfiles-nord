497cb55b960f816a2b654ca42929b513e2aaee60
Alex Mayer <amayer5125@gmail.com>
2019-06-11 01:07:49 -0400

    Update Polybar Config
    Follow theme guidelines closer
    Update config version

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,7 +4,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch status bar
 polybar status &
+
+echo "Bars launched..."
